c524e3b349cc856da380841c9f5b5e3f939dd16a
gutoPsilva <60670589+gutoPsilva@users.noreply.github.com>
2023-03-26 22:12:50 -0300

    30% edit

--- script.js
+++ script.js
@@ -3,22 +3,23 @@ limpar = document.getElementById('btnLim').addEventListener('click', limpar) // 
 let ulLi = document.getElementById('ulLis')
 
 function add(){
-    let txtTodo = document.getElementById("txtadd").value
     let novoLi = document.createElement('li')
-    var novoP = document.createElement('p')
+    var novoTxt = document.createElement('p')
+    let txtTodo = document.getElementById('txtadd').value
     let novoAbbrD = document.createElement('abbr')
     let novoAbbrE = document.createElement('abbr')
-    novoAbbrD.setAttribute('title', 'Remover')
-    novoAbbrE.setAttribute('title', 'Editar')
     novoAbbrD.innerHTML = '<button><span class="material-icons">delete</span></button>'
     novoAbbrE.innerHTML = '<button><span class="material-icons">edit</span></button>'
+    novoAbbrD.setAttribute('title', 'Remover')
+    novoAbbrE.setAttribute('title', 'Editar')
     novoLi.className = 'list'
-    novoP.className = 'para'
+    novoTxt.className = 'txt'
+
     if (txtTodo == 0){ // 0 --> VAZIO, espaço também conta como VAZIO, sacrifica a tarefa "0"
         window.alert('Digite uma tarefa antes de adicionar.')
     } else{
-        novoP.innerHTML = `${txtTodo}`
-        novoLi.appendChild(novoP)
+        novoTxt.innerHTML = txtTodo
+        novoLi.appendChild(novoTxt)
         novoLi.appendChild(novoAbbrD)
         novoLi.appendChild(novoAbbrE)
         ulLi.appendChild(novoLi)
@@ -28,30 +29,61 @@ function add(){
 
 ulLi.addEventListener('click', (e) => {
     // instanciar cada elemento e dizer quem é o seu parent, para remover tudo com o removeChildren
+    let par
     let abbr 
     let button 
     let span
+    let li
     // 3 if's pois o botão em si é composto desses 3 elementos, portanto, isso garante que em qualquer área do botão clicada, ele pegue o target do evento correto
     if(e.target.tagName == 'ABBR'){
         abbr = e.target
-        let li = abbr.parentNode
         if(abbr.innerHTML == '<button><span class="material-icons">delete</span></button>'){
+            li = abbr.parentNode
             ulLi.removeChild(li)
+        }else if(abbr.innerHTML == '<button><span class="material-icons">delete</span></button>'){
+            if(document.getElementById('txtadd').value == 0){
+                window.alert('Insira um texto na caixa antes de editar a tarefa.')
+            }else{
+                let edTxt = document.getElementById('txtadd').value
+                // window.alert(edTxt)
+                document.getElementById('txtadd').value = ''
+            }
         }
     }else if(e.target.tagName == 'BUTTON'){
         button = e.target
-        abbr = button.parentNode
-        let li =  abbr.parentNode
         if(button.innerHTML == '<span class="material-icons">delete</span>'){
+            abbr = button.parentNode
+            li =  abbr.parentNode
             ulLi.removeChild(li)
+        }
+        else if(button.innerHTML == '<span class="material-icons">edit</span>'){
+            if(document.getElementById('txtadd').value == 0){
+                window.alert('Insira um texto na caixa antes de editar a tarefa.')
+            }else {
+                let edTxt = document.getElementById('txtadd').value
+                // window.alert(edTxt)
+                par.innerHTML = edTxt
+                par = ulLi.getElementsByTagName(p[e])
+                document.getElementById('txtadd').value = ''
+            }
         }
     }else if(e.target.tagName == 'SPAN'){
         span = e.target
-        button = span.parentNode
-        abbr = button.parentNode
-        let li = abbr.parentNode
         if(span.textContent == 'delete'){
+            button = span.parentNode
+            abbr = button.parentNode
+            li = abbr.parentNode
             ulLi.removeChild(li)
+        }
+        else if(span.textContent == 'edit'){
+            if(document.getElementById('txtadd').value == 0){
+                window.alert('Insira um texto na caixa antes de editar a tarefa.')
+            }else{
+                let edTxt = document.getElementById('txtadd').value
+                p = ulLi.getElementsByTagName('p')
+                window.alert(edTxt)
+                document.getElementById('txtadd').value = ''
+            }
         }
     }
 })
